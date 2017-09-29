174d1568492aec0270b773e216f8dc3a2a7f6aac
Gustavo Pinho <gcdpinho@gmail.com>
2017-09-29 18:07:01 -0300

    fixed hover dos top-bar itens nas pages

---
+++ pages/contato/contato.js
@@ -0,0 +1,6 @@
+(function ($) {
+    "use strict";
+
+    $('.contato').css('color', '#327964')
+    $('.contato').css('border-bottom-color', '#920f12')
+})(jQuery); // End of use strict
--- pages/obras/obras.js
+++
@@ -1,6 +0,0 @@
-(function ($) {
-    "use strict";
-
-    $('.obras').css('color', '#327964')
-    $('.obras').css('border-bottom-color', '#920f12')
-})(jQuery); // End of use strict
